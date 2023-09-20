1c8c3632d2eca94edd6080c9569e5a9ab88facb7
Cornelius Brütt <cornelius.bruett@gmx.de>
2023-09-20 11:48:45 +0200

    added testing programm for the MPU board

---
+++ dev/mpu_test/mpu_test.ino
@@ -0,0 +1,51 @@
+/* Test programm for the GY-521 MPU Breakout Board
+*  
+* This programm will automatically find the GY521 board on the I2C bus 
+* and read out the x and y accel values. Going from these it will compute
+* the current angle and print it to Serial.
+*
+* Note: This requires the GY521 lib to be installed
+*
+*
+*
+*/
+#include "GY521.h"
+#include "math.h"
+
+GY521 mpu;
+
+float accY, accZ;
+float accAngle;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(0x68);
+  delay(100);
+  while(mpu.wakeup() == false) {
+    Serial.print(millis());
+    Serial.println("\tConnection to GY521 failed");
+    delay(1000);
+  }
+
+  mpu.setAccelSensitivity(1); //set to 4g
+  mpu.setGyroSensitivity(1); //set to 500dps
+}
+
+void loop() {
+  mpu.read(); //get new, fresh values
+  
+  //then, disect them into their components
+  accZ = mpu.getAccelZ();
+  accY = mpu.getAccelY();
+
+  accAngle = atan2(accY, accZ)*RAD_TO_DEG;
+  
+  //NaN check
+  if(isnan(accAngle));
+  else
+    Serial.println(accAngle);
+
+  delay(1000);
+}
