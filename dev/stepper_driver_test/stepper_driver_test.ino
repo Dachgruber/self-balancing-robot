4c305347995ee353ed9e9b569686e5a85849e8f8
Cornelius Brütt <cornelius.bruett@gmx.de>
2023-09-21 15:41:28 +0200

    added test for steppers

---
+++ dev/stepper_driver_test/stepper_driver_test.ino
@@ -0,0 +1,46 @@
+/* Test programm for the stepper driver boards
+*  This programm will test the integrity of the two stepper driver boards and motors
+*
+*  Note: The AccelStepper library has to be installed
+*
+*
+*
+*/
+
+#define dirPin1 3
+#define stepPin1 4
+#define dirPin2 6
+#define stepPin2 7
+
+#define stepsPerRevolution 200
+#define stepTime 1
+
+#include <AccelStepper.h>
+
+AccelStepper step1(AccelStepper::DRIVER,stepPin1,dirPin1);
+AccelStepper step2(AccelStepper::DRIVER,stepPin2,dirPin2);  
+
+const int maxSpeedLimit = 2000.0;
+
+void setup() {
+  step1.setMaxSpeed(maxSpeedLimit);
+  step2.setMaxSpeed(maxSpeedLimit);
+}
+
+void loop() {
+  
+
+  step1.setSpeed(maxSpeedLimit/5.0);
+  step2.setSpeed(maxSpeedLimit/5.0);
+  step1.runSpeed();
+  //step2.runSpeed();
+  //delay(2000);
+  
+
+  //step1.setSpeed(maxSpeedLimit/10.0);
+  //step2.setSpeed(maxSpeedLimit/10.0);
+  //step1.runSpeed();
+  //step2.runSpeed();
+  //delay(2000);
+   
+}
