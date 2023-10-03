6b9707142a96e9f81f57ff4988fdaeaa460c46b2
Cornelius Brütt <cornelius.bruett@gmx.de>
2023-10-03 20:01:12 +0200

    added ISR routine

--- dev/stepper_driver_test/stepper_driver_test.ino
+++ dev/stepper_driver_test/stepper_driver_test.ino
@@ -20,14 +20,17 @@
 AccelStepper step1(AccelStepper::DRIVER,stepPin1,dirPin1);
 AccelStepper step2(AccelStepper::DRIVER,stepPin2,dirPin2);  
 
-const int maxSpeedLimit = 2000.0;
+const int maxSpeedLimit = 32000.0;
 
 void setup() {
   step1.setMaxSpeed(maxSpeedLimit);
   step2.setMaxSpeed(maxSpeedLimit);
   
-  step1.setSpeed(1600);
-  step2.setSpeed(1600);
+
+  initPID();
+
+  step1.setSpeed(-4800);
+  step2.setSpeed(4800);
 
   //step1.runSpeed();
   //step2.runSpeed();
@@ -36,9 +39,11 @@ void setup() {
 
 void loop() {
   
-    
-  step1.runSpeed();
-  step2.runSpeed();
+  //Serial.println("Test");
+
+  //step2.setSpeed(-4800.00);
+  //step1.runSpeed();
+  //step2.runSpeed();
   //delay(2000);
   
 
@@ -48,4 +53,45 @@ void loop() {
   //step2.runSpeed();
   //delay(2000);
    
+}
+
+/* some register magic taken from instructables
+* LINK: https://www.instructables.com/Arduino-Self-Balancing-Robot-1/
+* 
+* This enables the internal Timer1 to wake up the PID routine every 5ms
+
+*  Calculations:
+*  System clock 16 Mhz and Prescalar 8;
+*  Timer 1 speed = 16Mhz/8 = 2Mhz    
+*  Pulse time = 1/2Mhz =  0.5us  
+*  Count up to = 0.05ms / 0.5us = 100 (so this is the value the OCR register should have)  
+*/
+void initPID(){
+  cli(); // disable global interrupts
+  TCCR1A = 0; //set entire TCCR1A register to 0, resetting the timer value
+  TCCR1B = 0; //same for TCCR1B
+
+  //set compare match register to set sample time 5ms
+  OCR1A = 100;
+  //turn on CTC mode
+  //TCCR1B |= (1 << WGM12);
+  //Set CS11 bit for prescaling by 8
+  //this will divide the clock signal with 8, speeding up  our timer
+  TCCR1B |= B00000010; //sets prescaling to 8
+  //TCCR1B |= (1 << CS11);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei(); //enable glonbal interrupts
+
+} 
+
+
+//the ISR will be called every 5ms
+ISR(TIMER1_COMPA_vect) {
+  TCNT1  = 0; //reset the timer 
+  Serial.print("ISR ");
+  step1.runSpeed();
+  step2.runSpeed();
+
 }
